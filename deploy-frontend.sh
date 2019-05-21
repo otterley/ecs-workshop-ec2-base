c5662cbf295b0fc780c7a0f7985e61dc41294540
Michael S. Fischer <fiscmi@amazon.com>
2019-05-21 14:37:37 -0700

    add deploy-frontend script

---
+++ deploy-frontend.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -ex
+
+STACK_NAME=ec2-base-$C9_USER
+
+target_group_arn=$(aws cloudformation describe-stacks --stack-name $STACK_NAME --query 'Stacks[0].Outputs[?OutputKey==`FrontEndTargetGroup`].OutputValue' --output text)
+vpc=$(aws cloudformation describe-stacks --stack-name $STACK_NAME --query 'Stacks[0].Outputs[?OutputKey==`VpcId`].OutputValue' --output text)
+
+cd ~/environment/ecsdemo-frontend
+envsubst < ecs-params.yml.template >ecs-params.yml
+
+ecs-cli compose --project-name ecsdemo-frontend service up \
+    --create-log-groups \
+    --target-group-arn $target_group_arn \
+    --private-dns-namespace service \
+    --enable-service-discovery \
+    --container-name ecsdemo-frontend \
+    --container-port 3000 \
+    --cluster-config fargate-demo \
+    --vpc $vpc
