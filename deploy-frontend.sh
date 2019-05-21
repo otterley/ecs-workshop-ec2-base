9fce1e8b7892ea67a0858b19de3cbae2c7ea8a69
Michael S. Fischer <fiscmi@amazon.com>
2019-05-21 14:50:36 -0700

    WIP

--- deploy-frontend.sh
+++ deploy-frontend.sh
@@ -4,18 +4,22 @@ set -ex
 
 source _common.sh
 
-target_group_arn=$(aws cloudformation describe-stacks --stack-name $STACK_NAME --query 'Stacks[0].Outputs[?OutputKey==`FrontEndTargetGroup`].OutputValue' --output text)
-vpc=$(aws cloudformation describe-stacks --stack-name $STACK_NAME --query 'Stacks[0].Outputs[?OutputKey==`VpcId`].OutputValue' --output text)
+export target_group_arn=$(aws cloudformation describe-stacks --stack-name $STACK_NAME --query 'Stacks[0].Outputs[?OutputKey==`FrontEndTargetGroup`].OutputValue' --output text)
+export vpc=$(aws cloudformation describe-stacks --stack-name $STACK_NAME --query 'Stacks[0].Outputs[?OutputKey==`VpcId`].OutputValue' --output text)
+export ecsTaskExecutionRole=$(aws cloudformation describe-stacks --stack-name fargate-demo --query 'Stacks[0].Outputs[?OutputKey==`ECSTaskExecutionRole`].OutputValue' --output text)
+export subnet_1=$(aws cloudformation describe-stacks --stack-name $STACK_NAME --query 'Stacks[0].Outputs[?OutputKey==`PrivateSubnetOne`].OutputValue' --output text)
+export subnet_2=$(aws cloudformation describe-stacks --stack-name $STACK_NAME --query 'Stacks[0].Outputs[?OutputKey==`PrivateSubnetTwo`].OutputValue' --output text)
+export subnet_3=$(aws cloudformation describe-stacks --stack-name $STACK_NAME --query 'Stacks[0].Outputs[?OutputKey==`PrivateSubnetThree`].OutputValue' --output text)
 
 cd ~/environment/ecsdemo-frontend
 envsubst < ecs-params.yml.template >ecs-params.yml
 
-ecs-cli compose --project-name ecsdemo-frontend service up \
+ecs-cli compose --project-name ecsdemo-frontend-$C9_USER service up \
     --create-log-groups \
     --target-group-arn $target_group_arn \
     --private-dns-namespace service \
     --enable-service-discovery \
-    --container-name ecsdemo-frontend \
+    --container-name ecsdemo-frontend-$C9_USER \
     --container-port 3000 \
     --cluster-config $CONFIG_NAME \
     --vpc $vpc
