d1d809826de0bd2503021e144d8c9fe3050703e2
Michael S. Fischer <fiscmi@amazon.com>
2019-05-21 14:41:25 -0700

    WIP

--- _common.sh
+++ _common.sh
@@ -1,3 +1,4 @@
-AWS_REGION=$(curl -s 169.254.169.254/latest/dynamic/instance-identity/document | jq -r .region)
+export AWS_REGION=$(curl -s 169.254.169.254/latest/dynamic/instance-identity/document | jq -r .region)
+
 STACK_NAME=ec2-base-$C9_USER
 CONFIG_NAME=ecs-workshop
